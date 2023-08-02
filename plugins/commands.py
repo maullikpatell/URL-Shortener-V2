4dd87d8a8f43734624749a3bad256901ca5c26b7
Maulik Patel <115573338+maullikpatell@users.noreply.github.com>
2023-08-03 05:20:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -69,6 +69,25 @@ async def start(c: Client, m: Message):
     )
 
 
+@Client.on_message(filters.command("kase_site") & filters.private)
+@private_use
+async def base_site_handler(bot, m: Message):
+    user_id = m.from_user.id
+    user = await get_user(user_id)
+    cmd = m.command
+    site = user["base_site"]
+    text = f"`/base_site (base_site)`\n\nCurrent base site: {site}\n\n EX: `/base_site shareus.in`\n\nAvailable base sites:\n{avl_web1}\nAnd All alternate sites to droplink.co"
+    if len(cmd) == 1:
+        return await m.reply(text=text, disable_web_page_preview=True)
+    elif len(cmd) == 2:
+        base_site = cmd[1].strip()
+        if not domain(base_site):
+            return await m.reply(text=text, disable_web_page_preview=True)
+        await update_user_info(user_id, {"base_site": base_site})
+        await m.reply("Base Site updated successfully")
+
+
+
 @Client.on_message(filters.command("help") & filters.private)
 @private_use
 async def help_command(c, m: Message):
